8d061b35a90dc7eb6dcadb855f8805150af460fa
Ariestellar <ahdrei47@gmail.com>
2020-08-18 16:46:13 +0300

    Initial commit

---
+++ Assets/RotatePlane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlane : MonoBehaviour
+{
+   [Range(0, 1)][SerializeField] private float _speed;
+
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.up, _speed);
+    }
+}
---
+++ Assets/SubjectInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SubjectInteraction : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Transform _hand;
+    private float _pushForce = 5000;
+    private float _telekinesisForce = 2000;
+    [SerializeField] private bool _isObjectInHand;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Arm")
+        {
+            _isObjectInHand = true;
+            _rigidbody.isKinematic = true;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_isObjectInHand == false)
+        {
+            transform.parent = null;
+            _rigidbody.useGravity = false;
+            _rigidbody.AddForce((_hand.position - transform.position).normalized * _telekinesisForce);
+        }
+        else
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.useGravity = true;
+            _rigidbody.AddForce(_hand.forward * _pushForce);
+        }               
+    }
+}
