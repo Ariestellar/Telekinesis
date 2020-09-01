b1ce866efcc40b5ff11615f34afa608b78e3950d
Ariestellar <ahdrei47@gmail.com>
2020-09-01 17:56:53 +0300

    MinorEdit

--- Assets/Scripts/TargetMovement.cs
+++ Assets/Scripts/TargetMovement.cs
@@ -7,25 +7,28 @@ public class TargetMovement : MonoBehaviour
 {
     [SerializeField] private Transform[] _targetPosition;
     private NavMeshAgent _agent;
-    private int _currentPointNumber;
+    [SerializeField] private int _currentPointNumber;
     
     private void Awake()
     {
-        _agent = GetComponent<NavMeshAgent>();
+        _agent = GetComponent<NavMeshAgent>();        
+    }
+
+    private void Start()
+    {
         _currentPointNumber = 0;
     }
 
     private void FixedUpdate()
     {        
-        _agent.destination = _targetPosition[_currentPointNumber].position;
-        if (_agent.remainingDistance == 0)
-        {            
-            _currentPointNumber += 1;
-            if (_currentPointNumber == _targetPosition.Length)
-            {
-                _currentPointNumber = 0;
-            }
-            Debug.Log("Установленна следующая точка №" + _currentPointNumber);
-        }
+        _agent.SetDestination(_targetPosition[_currentPointNumber].position);
+
+        if (_agent.remainingDistance == 0 && _agent.SetDestination(_targetPosition[_currentPointNumber].position))
+        {
+            Debug.Log("Установленна точка №" + _currentPointNumber);
+            Debug.Log("Расстояние:" + _agent.remainingDistance);
+            _currentPointNumber = (_currentPointNumber + 1) % _targetPosition.Length;
+            Debug.Log("Следующая точка установленна №" + _currentPointNumber);
+        }                
     }
 }
